fa1937776293c469a14ba503e9fe7055af84d6c3
Angela Bellò <angela.bello90@gmail.com>
2023-06-15 16:21:36 +0200

    added save and share

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,39 @@
-//VARIABILI
+/*---------------
+VARIABILI
+----------------*/
 const loader = document.querySelector(".loading");
 const modal = document.querySelector(".modal");
 const modalContent = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
+const characters = document.querySelectorAll(".character");
 const API_URL = "https://api.openai.com/v1/chat/completions";
 const MODEL = "gpt-3.5-turbo";
-const API_KEY = "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";
-// FUNZIONI
+const API_KEY = "";
+const saveBtn = document.querySelector("#save");
+const shareBtn = document.querySelector("#share");
+
+/*---------------
+ FUNZIONI
+----------------*/
+function getImgPath(nameCharacter) {
+  if (nameCharacter.includes(" ")) {
+    const spaceIndex = nameCharacter.indexOf(" ");
+    const path = `${nameCharacter.slice(0, spaceIndex)}-${nameCharacter.slice(
+      spaceIndex + 1
+    )}`;
+    return path.toLowerCase();
+  } else {
+    return nameCharacter.toLowerCase();
+  }
+}
+
 async function playCharacter(nameCharacter) {
-  //mostrare il loader
+  //1.mostrare il loader
+  //2.richiamare le API di OpenAI
   loader.classList.remove("loading-hidden");
-  //richiamare le API di OpenAI
-  // const action = "Saluta in modo iconico";
   const action = getRandomAction();
   const temperature = 0.7;
-  //recuperare la risposta
+  //3.recuperare la risposta
   const response = await fetch(API_URL, {
     method: "POST",
     headers: {
@@ -32,20 +51,28 @@ async function playCharacter(nameCharacter) {
       temperature: temperature,
     }),
   });
-  //interpretare la risposta in JSON
+
+  //4.interpretare la risposta in JSON
   const data = await response.json();
-  //compilare la modale con i dati ricevuti
+
+  //5.compilare la modale con i dati ricevuti
   const message = data.choices[0].message.content;
   modalContent.innerHTML = `
+  <div class="character">
+  <img src="/assets/img/${getImgPath(
+    nameCharacter
+  )}.png" alt="${nameCharacter}" />
+  </div>
   <h2>${nameCharacter}</h2>
   <p>${message}</p>
   <code>Character: ${nameCharacter}, action: ${action}, temperature: ${temperature}</code>
   `;
+
   //nascondere il loader e mostrare la modale
   loader.classList.add("loading-hidden");
   modal.classList.remove("modal-hidden");
 }
-
+//Scegliere una azione random
 function getRandomAction() {
   const actions = [
     "saluta nel tuo modo iconico",
@@ -59,8 +86,42 @@ function getRandomAction() {
   return actions[indexRandom];
 }
 
-// INIT
-const characters = document.querySelectorAll(".character");
+function saveQuote() {
+  html2canvas(modalContent).then(function (canvas) {
+    const dataURI = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = dataURI;
+    link.download = `quoteBOT-${getImgPath(
+      modal.querySelector("h2").innerText
+    )}.png`;
+    link.click();
+  });
+}
+
+async function shareQuote() {
+  //1.assemblare il messaggio da condividere
+  const character = modal.querySelector("h2").innerText;
+  const quote = modal.querySelector("p").innerText;
+
+  const text = `Senti cosa ha da dire ${character}: "${quote}" #QuoteBOT #BooleanCoodingWeek`;
+  //2. condividerlo con shareAPI
+  if (navigator.canShare) {
+    await navigator.share({ text: text });
+  } else {
+    console.log("Share API not supported");
+    fallbackShare();
+  }
+}
+
+function fallbackShare() {
+  //https://wa.me/?text=urlencodedtext
+  const href = `https://wa.me/?text=${encodeURIComponent(text)}`;
+  window.location.href = href;
+}
+
+/*---------------
+ INIZIALIZZAZIONE ED EVENTI
+----------------*/
 
 characters.forEach(function (element) {
   element.addEventListener("click", function () {
@@ -71,3 +132,6 @@ characters.forEach(function (element) {
 modalClose.addEventListener("click", function () {
   modal.classList.add("modal-hidden");
 });
+
+saveBtn.addEventListener("click", saveQuote);
+shareBtn.addEventListener("click", shareQuote);
